a5c83a0e3bbf0c0136a449d00071f6006ceaa25b
Adrian <contact@jsmastery.pro>
2025-05-31 18:59:43 +0200

    Implement Hero Scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ const App = () => {
 	<main>
 	 <Navbar />
 	 <Hero />
-	 <div className="h-dvh bg-black" />
 	</main>
  )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,28 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react'
 import { SplitText} from 'gsap/all'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+ 	const videoRef = useRef();
+	const videoTimelineRef = useRef();
+
+	const isMobile = useMediaQuery({ maxWidth: 767 });
+
  	useGSAP(() => {
 		const heroSplit = new SplitText('.title', { type: 'chars, words' });
 		const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
-		
+
 		heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
-		
+
 		gsap.from(heroSplit.chars, {
 		 yPercent: 100,
 		 duration: 1.8,
 		 ease: 'expo.out',
 		 stagger: 0.05
 		});
-		
+
 		gsap.from(paragraphSplit.lines, {
 		 opacity: 0,
 		 yPercent: 100,
@@ -24,7 +31,7 @@ const Hero = () => {
 		 stagger: 0.06,
 		 delay: 1,
 		});
-		
+
 		gsap.timeline({
 		 scrollTrigger: {
 			trigger: '#hero',
@@ -35,25 +42,46 @@ const Hero = () => {
 		})
 		 .to('.right-leaf', { y: 200 }, 0)
 		 .to('.left-leaf', { y: -200 }, 0)
+
+		const startValue = isMobile ? 'top 50%' : 'center 60%';
+		const endValue = isMobile ? '120% top' : 'bottom top';
+
+		// Video animation timeline
+		// Create the timeline with a default duration
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: 'video',
+				start: startValue,
+				end: endValue,
+				scrub: true,
+			 	pin: true,
+			}
+		})
+
+		videoRef.current.onloadedmetadata = () => {
+		 tl.to(videoRef.current, {
+			currentTime: videoRef.current.duration
+		 })
+		}
 	}, []);
- 
+
  return (
 	<>
 		<section id="hero" className="noisy">
 	 		<h1 className="title">MOJITO</h1>
-		 
+
 		 <img
 		 	src="/images/hero-left-leaf.png"
 			alt="left-leaf"
 			className="left-leaf"
 		 />
-		 
+
 		 <img
 		 	src="/images/hero-right-leaf.png"
 			alt="right-leaf"
 			className="right-leaf"
 		 />
-		 
+
 		 <div className="body">
 			<div className="content">
 			 <div className="space-y-5 hidden md:block">
@@ -62,7 +90,7 @@ const Hero = () => {
 				 Sip the Spirit <br /> of Summer
 				</p>
 			 </div>
-			 
+
 			 <div className="view-cocktails">
 				<p className="subtitle">
 				 Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses.
@@ -72,6 +100,16 @@ const Hero = () => {
 			</div>
 		 </div>
 		</section>
+
+	 <div className="video absolute inset-0">
+		<video
+		 ref={videoRef}
+		 muted
+		 playsInline
+		 preload="auto"
+		 src="/videos/output.mp4"
+		/>
+	 </div>
 	</>
  )
 }
